bfd95c46c5e98644a96377bea61c804bdf9531f7
Eduardo Augusto Klosowski <eduardo.klosowski@univille.br>
2017-04-03 19:16:42 -0300

    Ordenacao

---
+++ src/estd/Sorter.java
@@ -0,0 +1,18 @@
+package estd;
+
+import java.util.Arrays;
+
+public class Sorter {
+    public void sort(int[] a) {
+        int n = a.length;
+        for (int i = 1; i < n; i++) {
+            int cur = a[i];
+            int j = i - 1;
+            while ((j >= 0) && (a[j] > cur)) {
+                a[j + 1] = a[j--];
+            }
+            a[j + 1] = cur;
+        }
+        System.out.println(Arrays.toString(a));
+    }
+}
